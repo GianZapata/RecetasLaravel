51b56000548a677fe152fefb99829a89c9b6cc71
GianZapata <elgianmc@hotmail.com>
2021-11-14 21:31:09 -0600

    CORS

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
 
 use App\Http\Controllers\CategoriasController;
 use App\Http\Controllers\InicioController;
